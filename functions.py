88c69353e06e6364c6994dfd028a239afe576444
Tonatiuh Navarro <if722399@iteso.mx>
2022-11-01 13:27:19 -0600

    Firsts steps modeling

--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
 import numpy as np
 
-def matrix_format(images_set):
+def matrix_format(images_set,flaten: bool = False):
+    if bool:
+        function = lambda x: np.asarray(x.convert('L')).flatten()
+        return list(map(function,images_set))
     return list(map(lambda x: np.asarray(x.convert('L')),images_set))
+
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 import numpy as np
 from PIL import Image
 from io import BytesIO
@@ -5,6 +6,8 @@ import matplotlib.pyplot as plt
 from data.data import test_neumonia, test_normal, train_neumonia, train_normal
 import functions as fn
 import cv2
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 plt.style.use('ggplot')
 
 
@@ -16,3 +19,36 @@ abs_train = sets['train_neumonia']+sets['train_normal']
 abs_test = sets['test_neumonia']+sets['test_normal']
 train_neumonia_prop = np.round(sets['train_neumonia']/abs_train,4)*100
 test_neumonia_prop = np.round(sets['test_neumonia']/abs_test,4)*100
+
+
+
+
+# Dataset generator:
+train_datagen = ImageDataGenerator(rescale = 1/255)
+test_datagen = ImageDataGenerator(rescale = 1/255, validation_split = 0.2)
+
+train_generator = ImageDataGenerator(rescale = 1/255,
+                                     target_size=(350,350),
+                                     batch_size=10,
+                                     class_mode='gategorical',
+                                     validation_split = 0.2)
+
+
+
+
+# Modelo
+model_base = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(350,350,1)),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(2, activation='softmax')
+])
+
+
+#model_base.summary()
+
+model_base.compile(optimizer = 'adam',
+                    loss = 'categorical_crossentropy',
+                    metrics=['accuracy'])
+
+history = model_base.fit()
