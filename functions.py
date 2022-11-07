2479ce484be1fc30340f0f1efba1ed958e0810f0
SR_CienciaDeDatos <jose.haces@iteso.mx>
2022-11-07 17:42:40 -0600

    Trained Model
    Creación del Notebook 'Modelo.ipynb' en donde se crea el modelo y carga del mismo en la carpeta 'model'. Cambios menores a la estructura del repo.

--- functions.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def matrix_format(images_set,flaten: bool = False):
-    if bool:
-        function = lambda x: np.asarray(x.convert('L')).flatten()
-        return list(map(function,images_set))
-    return list(map(lambda x: np.asarray(x.convert('L')),images_set))
-
-
